07f4b7cef73816b85fa4353718b27d54f0ee1d76
Lars Van Cauter <larsvancauter@gmail.com>
2021-12-17 08:22:52 +0100

    adding gitignores

--- src/initproj.rs
+++ src/initproj.rs
@@ -1,12 +1,28 @@
+use std::fs;
 use std::process::Command;
 use std::string::String;
 
 pub fn init_project(lang: String, nvim: bool, giturl: String) {
     match lang.as_str() {
-        "python" | "py" => setup_python(nvim),
+        "python" | "py" => {
+            setup_python(nvim);
+            // setup_git(giturl)
+        }
         _ => println!("Language not supported yet"),
     }
-    println!("{}", "executing git stuff");
+}
+
+fn setup_python(nvim: bool) {
+    let gitignore: String =
+        fs::read_to_string("src/ignores/python_gitignore").expect("Couldn't read file");
+    fs::write(".gitignoretest", gitignore).expect("Couldn't create file");
+    // Command::new("echo")
+    //     .arg("hello from setup_python")
+    //     .status()
+    //     .expect("failed to execute process");
+}
+
+fn setup_git(giturl: String) {
     if giturl == "" {
         Command::new("git").arg("init");
         Command::new("git").arg("commit").arg("-m").arg("initing");
@@ -16,39 +32,31 @@ pub fn init_project(lang: String, nvim: bool, giturl: String) {
             .arg("init")
             .status()
             .expect("failed to execute process");
-
-        // Command::new("git")
-        //     .arg("commit")
-        //     .arg("-m")
-        //     .arg("initing")
-        //     .status()
-        //     .expect("failed to execute process");
-        // Command::new("git")
-        //     .arg("branch")
-        //     .arg("-M")
-        //     .arg("root")
-        //     .status()
-        //     .expect("failed to execute process");
-        // Command::new("git")
-        //     .arg("remote")
-        //     .arg("add")
-        //     .arg("origin")
-        //     .arg(giturl)
-        //     .status()
-        //     .expect("failed to execute process");
-        // Command::new("git")
-        //     .arg("push")
-        //     .arg("-u")
-        //     .arg("origin")
-        //     .arg("root")
-        //     .status()
-        //     .expect("failed to execute process");
+        Command::new("git")
+            .arg("commit")
+            .arg("-m")
+            .arg("initing")
+            .status()
+            .expect("failed to execute process");
+        Command::new("git")
+            .arg("branch")
+            .arg("-M")
+            .arg("root")
+            .status()
+            .expect("failed to execute process");
+        Command::new("git")
+            .arg("remote")
+            .arg("add")
+            .arg("origin")
+            .arg(giturl)
+            .status()
+            .expect("failed to execute process");
+        Command::new("git")
+            .arg("push")
+            .arg("-u")
+            .arg("origin")
+            .arg("root")
+            .status()
+            .expect("failed to execute process");
     }
-}
-
-fn setup_python(nvim: bool) {
-    Command::new("echo")
-        .arg("hello from setup_python")
-        .status()
-        .expect("failed to execute process");
 }
